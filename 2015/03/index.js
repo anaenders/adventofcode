51fa1b69c8e6f5c9e56714d0bba0fa31386cdeb9
Ana Enders <ana.enders@gmail.com>
2017-06-04 17:59:37 -0400

    Fixed problem 3.2 by using ES6 classes

--- 2015/03/index.js
+++ 2015/03/index.js
@@ -2,52 +2,60 @@
 
 const fs = require('fs')
 const directions = fs.readFileSync('./input.txt', 'utf8')
-let coordinates = "0,0"
-let houses = {}
-let count = 0
 
-houses[coordinates] = 1
-count += 1
+const VisitHouses = class {
+    constructor() {
+        this.coordinates = "0,0"
+        this.houses = {}
+        this.houses[this.coordinates] = 1
+        this.count = 1
+    }
 
-const calcMoves = (startPosition, moveBy) => {
-    let position = {x: 0, y:0}
+    getCount() {
+        return this.count
+    }
 
-    for(startPosition; startPosition < directions.length; startPosition += moveBy) {
-        switch(directions[startPosition]) {
-            case "^":
-                position.y += 1
-                break
-            case "v":
-                position.y -= 1
-                break
-            case ">":
-                position.x += 1
-                break
-            case "<":
-                position.x -= 1
-                break
+    setMoves(startPosition, moveBy) {
+        let position = {x: 0, y:0}
+
+        for(startPosition; startPosition < directions.length; startPosition += moveBy) {
+            switch(directions[startPosition]) {
+                case "^":
+                    position.y += 1
+                    break
+                case "v":
+                    position.y -= 1
+                    break
+                case ">":
+                    position.x += 1
+                    break
+                case "<":
+                    position.x -= 1
+                    break
+            }
+
+            this.coordinates = `${position.x},${position.y}`
+
+            if(!this.houses[this.coordinates]) {
+                this.houses[this.coordinates] = 1
+                this.count += 1
+            }
         }
 
-        coordinates = `${position.x},${position.y}`
-
-        if(!houses[coordinates]) {
-            houses[coordinates] = 1
-            count += 1
-        }
     }
 }
 
 // Part 1
-// Comment out the code in part 2 if you want accurate results for part 1
 
-calcMoves(0, 1)
+const part1Solution = new VisitHouses()
+part1Solution.setMoves(0, 1)
 
-console.log(`Santa delivered at least one gift to ${count} houses.`)
+console.log(`Santa delivered at least one gift to ${part1Solution.getCount()} houses.`)
 
 // Part 2
-// Comment out the code in part 1 if you want accurate results for part 2
 
-calcMoves(0, 2)
-calcMoves(1, 2)
+const part2Solution = new VisitHouses()
+part2Solution.setMoves(0, 2)
+part2Solution.setMoves(1, 2)
 
-console.log(`Santa and Robo-santa delivered at least one gift to ${count} houses.`)
+console.log(`Santa and Robo-santa delivered at least one gift to ${part2Solution.getCount()} houses.`)
