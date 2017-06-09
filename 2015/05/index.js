aa52d6039d7a78eedd0e4c37478645d579d28537
Ana Enders <ana.enders@gmail.com>
2017-06-08 23:11:58 -0400

    Finally got 5.2 working

--- 2015/05/index.js
+++ 2015/05/index.js
@@ -3,14 +3,9 @@ const fs = require('fs');
 const dimensions = fs.readFileSync('./input.txt', 'utf8');
 const lines = dimensions.split('\n');
 let line = [];
-let atLeast3Vowels;
-let doubleLetters;
-let badSubstring;
-let niceStrings = 0;
-let newNiceStrings = 0;
 
 const checkForVowels = (line) => {
-    atLeast3Vowels = false;
+    let atLeast3Vowels = false;
     let vowels = 0;
     for(let i = 0; i < line.length; i++) {
         switch(line[i]) {
@@ -34,56 +29,46 @@ const checkForVowels = (line) => {
     if(vowels >= 3) {
         atLeast3Vowels = true;
     }
+    return atLeast3Vowels;
 }
 
 const checkForRepeats = (line) => {
-    doubleLetters = false;
+    let doubleLetters = false;
     for(let i = 0; i < line.length - 1; i++) {
         if(line[i] === line[i+1]) {
             doubleLetters = true;
         }
     }
+    return doubleLetters;
 }
 
 const checkForBadSubstrings = (line) => {
-    badSubstring = false;
+    let badSubstring = false;
     let substring = "";
     for(let i = 0; i < line.length - 1; i++) {
         substring = `${line[i]}${line[i+1]}`;
-        switch(substring) {
-            case "ab":
-                badSubstring = true;
-                break;
-            case "cd":
-                badSubstring = true;
-                break;
-            case "pq":
-                badSubstring = true;
-                break;
-            case "xy":
-                badSubstring = true;
-                break;
+        if(substring === "ab" || substring === "cd" || substring === "pq" || substring === "xy") {
+            badSubstring = true;
         }
     }
+    return badSubstring;
 }
 
 // Part 1
-
-for(let i = 0; i < lines.length; i++) {
-    line = lines[i].split('');
-
-    checkForVowels(line);
-    checkForRepeats(line);
-    checkForBadSubstrings(line);
-    if(atLeast3Vowels && doubleLetters && !badSubstring) {
-        niceStrings ++;
+const solutionPart1 = () => {
+    let niceStrings = 0;
+    for(let i = 0; i < lines.length; i++) {
+        line = lines[i].split('');
+        if(checkForVowels(line) && checkForRepeats(line) && !checkForBadSubstrings(line)) {
+            niceStrings ++;
+        }
     }
+    return console.log(`There are ${niceStrings} nice strings.`);
 }
 
-console.log(`There are ${niceStrings} nice strings.`);
+solutionPart1();
 
 // Part 2
-
 const findPairs = (line) => {
     let pair = [];
     let match = false;
@@ -94,37 +79,41 @@ const findPairs = (line) => {
         for(let k = 0; k < pair.length; k++) {
             if(k != j && (k != j - 1) && (k != j+1)) {
                 if(pair[j] === pair[k]) {
-                    // console.log(`${pair[j]} and ${pair[k]} are a pair`);
-                    return match = true;
+                    match = true;
                 }
             }
         }
     }
+    return match;
 }
 
 const findPalindromes = (line) => {
-    let triplet = [];
+    let findMatch = [];
     let match = false;
-    for(let i = 0; i < line.length - 3; i++) {
-        triplet[i] = [[line[i], line[i+1], line[i+2]]];
+    for(let i = 0; i < line.length; i++) {
+        findMatch[i] = line[i];
     }
-    for(let j = 0; j < triplet.length; j++) {
-        if(triplet[j][0][0] === triplet[j][0][2]) {
-            // console.log(`${triplet[j]} is a palindrome`);
-            return match = true;
+    for(let j = 0; j < findMatch.length; j++) {
+        if(findMatch[j] === findMatch[j+2]) {
+            match = true;
         }
     }
+    return match;
 }
 
-for(let i = 0; i < lines.length; i++) {
-    line = lines[i].split('');
-    let pairs = findPairs(line);
-    let palindromes = findPalindromes(line);
-
-    if(pairs && palindromes) {
-        newNiceStrings ++;
-        // console.log(`${lines[i]} is a nice string.`);
+const solutionPart2 = () => {
+    let niceStrings = 0;
+    let pairs = "";
+    let palindromes = "";
+    for(let i = 0; i < lines.length; i++) {
+        line = lines[i].split('');
+        pairs = findPairs(line);
+        palindromes = findPalindromes(line);
+        if(pairs && palindromes) {
+            niceStrings ++;
+        }
     }
+    return console.log(`There are ${niceStrings} nice strings.`);
 }
 
-console.log(`There are ${newNiceStrings} new nice strings.`);
+solutionPart2();
