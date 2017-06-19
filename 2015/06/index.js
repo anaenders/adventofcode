5c2ecf4a231840b31db2a96bd505ebb35572d50e
Ana Enders <ana.enders@gmail.com>
2017-06-19 02:06:26 -0400

    6.2 complete and converted to a class

--- 2015/06/index.js
+++ 2015/06/index.js
@@ -3,57 +3,121 @@ const fs = require('fs');
 const dimensions = fs.readFileSync('./input.txt', 'utf8');
 const instructions = dimensions.split('\n');
 let line = [];
-let litUp = {};
-let lightsLit = 0;
-let coordinate = "";
-let topCorner = "";
-let bottomCorner = "";
 
-const turnOn = (bottomCorner, topCorner) => {
-    for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
-        for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
-            coordinate = `${i},${j}`;
-            if(!litUp[coordinate] || litUp[coordinate] === "off") {
-                litUp[coordinate] = "on";
-                lightsLit ++;
+const LightDisplay = class {
+    constructor() {
+        this.line = [];
+        this.lights = {};
+        this.lit = 0;
+        this.totalBrightness = 0;
+        this.coordinates = "";
+    }
+
+    turnOn(bottomCorner, topCorner) {
+        for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
+            for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
+                this.coordinates = `${i},${j}`;
+                if(!this.lights[this.coordinates] || this.lights[this.coordinates] === "off") {
+                    this.lights[this.coordinates] = "on";
+                    this.lit ++;
+                }
             }
         }
     }
-}
 
-const turnOff = (bottomCorner, topCorner) => {
-    for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
-        for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
-            coordinate = `${i},${j}`;
-            if(litUp[coordinate] && litUp[coordinate] === "on") {
-                litUp[coordinate] = "off";
-                lightsLit --;
-            } else {
-                litUp[coordinate] = "off";
+    turnOff(bottomCorner, topCorner) {
+        for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
+            for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
+                this.coordinates = `${i},${j}`;
+                if(this.lights[this.coordinates] && this.lights[this.coordinates] === "on") {
+                    this.lights[this.coordinates] = "off";
+                    this.lit --;
+                } else {
+                    this.lights[this.coordinates] = "off";
+                }
             }
         }
     }
-}
 
-const toggle = (bottomCorner, topCorner) => {
-    for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
-        for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
-            coordinate = `${i},${j}`;
-            if(litUp[coordinate] === "on") {
-                litUp[coordinate] = "off";
-                lightsLit --;
-            }
-            else if(litUp[coordinate] === "off") {
-                litUp[coordinate] = "on";
-                lightsLit ++;
-            }
-            else {
-                litUp[coordinate] = "on";
-                lightsLit ++;
+    toggle(bottomCorner, topCorner) {
+        for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
+            for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
+                this.coordinates = `${i},${j}`;
+                if(this.lights[this.coordinates] === "on") {
+                    this.lights[this.coordinates] = "off";
+                    this.lit --;
+                }
+                else if(this.lights[this.coordinates] === "off") {
+                    this.lights[this.coordinates] = "on";
+                    this.lit ++;
+                }
+                else {
+                    this.lights[this.coordinates] = "on";
+                    this.lit ++;
+                }
             }
         }
     }
+
+    getLightsLit() {
+        return this.lit;
+    }
+
+    increaseBrightness(bottomCorner, topCorner) {
+        let count = 0;
+        for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
+            for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
+                this.coordinates = `${i},${j}`;
+                if(this.lights[this.coordinates]) {
+                    this.count = this.lights[this.coordinates];
+                    this.lights[this.coordinates] = this.count + 1;
+                } else {
+                    this.lights[this.coordinates] = 1;
+                }
+                this.totalBrightness ++;
+            }
+        }
+    }
+
+    decreaseBrightness(bottomCorner, topCorner) {
+        let count = 0;
+        for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
+            for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
+                this.coordinates = `${i},${j}`;
+                if(this.lights[this.coordinates] && this.lights[this.coordinates] > 0) {
+                    this.count = this.lights[this.coordinates];
+                    this.lights[this.coordinates] = this.count - 1;
+                    this.totalBrightness --;
+                } else {
+                    this.lights[this.coordinates] = 0;
+                }
+            }
+        }
+    }
+
+    toggleBrightness(bottomCorner, topCorner) {
+        let count = 0;
+        for(let i = bottomCorner[0]; i <= topCorner[0]; i++) {
+            for(let j = bottomCorner[1]; j <= topCorner[1]; j++) {
+                this.coordinates = `${i},${j}`;
+                if(this.lights[this.coordinates]) {
+                    this.count = this.lights[this.coordinates];
+                    this.lights[this.coordinates] = this.count + 2;
+                } else {
+                    this.lights[this.coordinates] = 2;
+                }
+                this.totalBrightness += 2;
+            }
+        }
+    }
+
+    getTotalBrightness() {
+        return this.totalBrightness;
+    }
 }
+
+// Part 1
+const part1 = new LightDisplay();
 
 const solutionPart1 = () => {
     for(let i = 0; i < instructions.length; i++) {
@@ -62,17 +126,42 @@ const solutionPart1 = () => {
             bottomCorner = line[2].split(',').map(Number);
             topCorner = line[4].split(',').map(Number);
             if(line[1] === "on") {
-                turnOn(bottomCorner,topCorner);
+                part1.turnOn(bottomCorner,topCorner);
             } else {
-                turnOff(bottomCorner,topCorner);
+                part1.turnOff(bottomCorner,topCorner);
             }
         } else {
             bottomCorner = line[1].split(',').map(Number);
             topCorner = line[3].split(',').map(Number);
-            toggle(bottomCorner,topCorner);
+            part1.toggle(bottomCorner,topCorner);
         }
     }
-    console.log(`${lightsLit} lights are lit.`);
+    console.log(`${part1.getLightsLit()} lights are lit.`);
 }
 
 solutionPart1();
+
+// Part 2
+const part2 = new LightDisplay();
+
+const solutionPart2 = () => {
+    for(let i = 0; i < instructions.length; i++) {
+        line = instructions[i].split(' ');
+        if(line[0] === "turn") {
+            bottomCorner = line[2].split(',').map(Number);
+            topCorner = line[4].split(',').map(Number);
+            if(line[1] === "on") {
+                part2.increaseBrightness(bottomCorner,topCorner);
+            } else {
+                part2.decreaseBrightness(bottomCorner,topCorner);
+            }
+        } else {
+            bottomCorner = line[1].split(',').map(Number);
+            topCorner = line[3].split(',').map(Number);
+            part2.toggleBrightness(bottomCorner,topCorner);
+        }
+    }
+    console.log(`The total brightness of all lights combined is ${part2.getTotalBrightness()}.`);
+}
+
+solutionPart2();
